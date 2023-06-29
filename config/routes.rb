dafc7b61b1fad7e19a90daf81a202fd532285dd0
Rea <andrea.solisg@gmail.com>
2023-06-29 18:11:23 +0200

    rigth address

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  resources :addresses
   resources :user_roles
   resources :roles
   resources :cart_courses
   resources :carts
   resources :orders
   resources :courses
-  resources :addresses
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
