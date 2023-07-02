9b330eec9d28a72e9fbc65af322bbf36a23312bf
Rea <andrea.solisg@gmail.com>
2023-07-02 18:26:11 +0200

    get teachers list, and teachers personal info

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :orders
   resources :courses
   devise_for :users
+
+  resources :teachers, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
