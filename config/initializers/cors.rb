6c3a66a2bbcc92726ad31290384529e91d754e65
Rea <andrea.solisg@gmail.com>
2023-07-03 17:31:30 +0200

    allowing the front to make requests

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://profs-app-front-production.vercel.app/"
+    origins "https://profs-app-front.vercel.app/"
 
     resource "*",
       headers: :any,
